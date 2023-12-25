84d52c207796dfe6d1496e00a2bbd2aeb4142fb3
saifulislam05 <saiful.bijulia@gmail.com>
2023-12-25 20:28:31 +0530

    header section done

---
+++ src/App.js
@@ -0,0 +1,15 @@
+
+import './App.css';
+import Body from './Components/Body/Body';
+import Header from './Components/Header/Header';
+
+function App() {
+  return (
+    <div className="App">
+      <Header />
+      <Body/>
+    </div>
+  );
+}
+
+export default App;
---
+++ src/Components/Body/Body.js
@@ -0,0 +1,13 @@
+// Body.js
+import React from "react";
+
+const Body = () => {
+ 
+  return (
+    <div className="w-11/12 mx-auto flex flex-col justify-center px-5 py-2 lg:px-auto lg:pt-12">
+      
+    </div>
+  );
+};
+
+export default Body;
