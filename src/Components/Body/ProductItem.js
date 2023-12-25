dbd0abdd139261032aa24a312992dfa6275c55ca
saifulislam05 <saiful.bijulia@gmail.com>
2023-12-25 21:34:23 +0530

    project done

--- src/Components/Body/ProductItem.js
+++ src/Components/Body/ProductItem.js
@@ -1,31 +1,42 @@
 import React from 'react'
 
-const ProductItem = ({ data }) => {
-    
-    const { name, src, price_string, price_number, quantity } = data;
+const ProductItem = ({ data, updateQuantity, removeProduct }) => {
+  const { id, name, src, price_string, quantity } = data;
+
+  const handleIncrease = () => {
+    updateQuantity(id, quantity + 1);
+  };
+
+  const handleDecrease = () => {
+    if (quantity > 0) {
+      updateQuantity(id, quantity - 1);
+    }
+  };
+
+  const handleRemove = () => {
+    removeProduct(id);
+  };
   return (
-    <div class="w-full flex shadow-2xl  rounded-lg">
+    <div className="w-full flex shadow-2xl  rounded-lg">
       <div className="w-1/2 flex gap-4">
-        <img
-          src={src}
-          alt={name}
-          className="w-20"
-        />
+        <img src={src} alt={name} className="w-20" />
         <div className="text-left">
           <h5>{name}</h5>
-          <span class="item-price">{price_string}</span>
-          <button class="text-red-600">remove</button>
+          <span className="item-price">{price_string}</span>
+          <button onClick={handleRemove} className="text-red-600">
+            remove
+          </button>
         </div>
       </div>
       <div className="w-1/2 flex flex-col items-end">
-        <div className='flex flex-col items-center justify-between'>
-          <button class="amount-btn">
+        <div className="flex flex-col items-center justify-between">
+          <button onClick={handleIncrease} className="amount-btn">
             <svg
               stroke="currentColor"
               fill="currentColor"
-              stroke-width="0"
+              strokeWidth="0"
               viewBox="0 0 448 512"
-              class="amount-icon"
+              className="amount-icon"
               height="1em"
               width="1em"
               xmlns="http://www.w3.org/2000/svg"
@@ -33,14 +44,14 @@ const ProductItem = ({ data }) => {
               <path d="M240.971 130.524l194.343 194.343c9.373 9.373 9.373 24.569 0 33.941l-22.667 22.667c-9.357 9.357-24.522 9.375-33.901.04L224 227.495 69.255 381.516c-9.379 9.335-24.544 9.317-33.901-.04l-22.667-22.667c-9.373-9.373-9.373-24.569 0-33.941L207.03 130.525c9.372-9.373 24.568-9.373 33.941-.001z"></path>
             </svg>
           </button>
-                  <span class="amount">{ quantity}</span>
-          <button class="amount-btn">
+          <span className="amount">{quantity}</span>
+          <button onClick={handleDecrease} className="amount-btn">
             <svg
               stroke="currentColor"
               fill="currentColor"
-              stroke-width="0"
+              strokeWidth="0"
               viewBox="0 0 448 512"
-              class="amount-icon"
+              className="amount-icon"
               height="1em"
               width="1em"
               xmlns="http://www.w3.org/2000/svg"
@@ -52,6 +63,6 @@ const ProductItem = ({ data }) => {
       </div>
     </div>
   );
-}
+};
 
 export default ProductItem
--- src/Components/Body/Products.js
+++ src/Components/Body/Products.js
@@ -1,28 +1,55 @@
-import React from 'react'
-import ProductItem from './ProductItem';
-import ProductList from '../../Data/ProductList.json'
+import React, { useState } from "react";
+import ProductItem from "./ProductItem";
+import ProductList from "../../Data/ProductList.json";
 
 const Products = () => {
-    return (
-        <div className="w-full p-3 flex flex-col">
+  const [cart, setCart] = useState(ProductList);
 
-            <div className='flex flex-col gap-8'>
-                {
-                ProductList.map((data) => {
-                  return  <ProductItem key={data.id} data={data} />
-                })
-            }
-            </div>
-            
-      
-
-        <div className="flex justify-between items-center w-full mt-6 py-3 border-t-2 border-gray-500">
-          <p className="text-xl font-bold text-zinc-700">Total</p>
-          <p className="text-xl font-bold text-primary">$4196</p>
-        </div>
-        <button className="btn btn-danger btn-sm w-fit mx-auto">Clear Cart</button>
-      </div>
+  const updateQuantity = (productId, newQuantity) => {
+    setCart((prevCart) =>
+      prevCart.map((product) =>
+        product.id === productId
+          ? { ...product, quantity: newQuantity }
+          : product
+      )
     );
-}
+  };
 
-export default Products
+  const removeProduct = (productId) => {
+    setCart((prevCart) =>
+      prevCart.filter((product) => product.id !== productId)
+    );
+  };
+
+  const calculateTotal = () => {
+    return cart.reduce(
+      (total, product) => total + product.price_number * product.quantity,
+      0
+    );
+  };
+
+  return (
+    <div className="w-full p-3 flex flex-col">
+      <div className="flex flex-col gap-8">
+        {cart.map((data) => (
+          <ProductItem
+            key={data.id}
+            data={data}
+            updateQuantity={updateQuantity}
+            removeProduct={removeProduct}
+          />
+        ))}
+      </div>
+
+      <div className="flex justify-between items-center w-full mt-6 py-3 border-t-2 border-gray-500">
+        <p className="text-xl font-bold text-zinc-700">Total</p>
+        <p className="text-xl font-bold text-primary">${calculateTotal()}</p>
+      </div>
+      <button className="btn btn-danger btn-sm w-fit mx-auto">
+        Clear Cart
+      </button>
+    </div>
+  );
+};
+
+export default Products;
